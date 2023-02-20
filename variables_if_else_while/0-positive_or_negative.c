1859d4121a7edd6191ae8a8ba455965fd4ec8a07
truuue <noahvernhet@gmail.com>
2023-02-20 04:45:42 -0800

    Complete the source code in order to print the last digit of the number stored in the variable n.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,29 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ *main - Determine if the number is positive, negative or zero
+ *Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n");
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n");
-	}
-	else
-	{
-		printf("%d is negative\n");
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
