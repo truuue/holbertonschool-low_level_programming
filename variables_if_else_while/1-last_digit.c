e43ea5c41174044709b96bd9fdf0ddada2102a02
truuue <noahvernhet@gmail.com>
2023-02-21 01:27:02 -0800

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ *main - Entry point
+ *Return - Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
