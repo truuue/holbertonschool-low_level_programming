ded50ef91129b50dc3c5285585177e42689dfc47
truuue <noahvernhet@gmail.com>
2023-02-21 01:28:45 -0800

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 /**
  *main - Entry point
- *Return - Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 /* betty style doc for function main goes there */
 int main(void)
