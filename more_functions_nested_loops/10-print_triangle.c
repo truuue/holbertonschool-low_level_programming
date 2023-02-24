1ced1c0e234dc2b4e3ea38668c0cf1e2a823f6d2
truuue <noahvernhet@gmail.com>
2023-02-24 12:11:06 -0800

    Write a function that prints a triangle, followed by a new line

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - print square logic ?
+ * @beta: size
+ */
+void print_triangle(int beta)
+{
+	int alpha, sigma;
+	if (beta > 0)
+	{
+		for (alpha = 1; alpha < beta; alpha++)
+		{
+			for (sigma = beta - alpha; sigma > 0; sigma--)
+				putchar(' ');
+
+			for (sigma = 0; sigma < beta; sigma++)
+				putchar('#');
+
+			if (alpha == beta)
+				continue;
+
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
