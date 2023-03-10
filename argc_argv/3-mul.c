937d037536dc3ccc8df8e4c193b760044a40fad9
truuue <noahvernhet@gmail.com>
2023-03-10 03:16:36 -0800

    test to fix execute problem

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,10 +13,8 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (1);
+		return (0);
 	}
-	else
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
