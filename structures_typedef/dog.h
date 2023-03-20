63fcce54dc0272b1f38847acd6424880d6112956
truuue <noahvernhet@gmail.com>
2023-03-20 02:36:43 -0700

    Adding the dog.h file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,24 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - structure
- * @name: char string
- * @age: int
- * @owner: char string
- */
+	/**
+	 * struct dog - struct
+	 * @name: char string
+	 * @age: int
+	 * @owner: char string
+	 */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
 
-typedef struct dog dog_t;
+	typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
 
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+	#endif
