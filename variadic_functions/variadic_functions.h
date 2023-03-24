4f26e0e2f22aa6f1b7bb5764e65b7cba1744b47d
truuue <noahvernhet@gmail.com>
2023-03-24 06:51:34 -0700

    Adding the Prototype and a struct for Print_all task

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * print_type - struct for print_all
+ * @type: print type
+ * @f: func to print the type
+ */
+
+typedef struct print_type
+{
+	char type;
+	void (*f)();
+} print_type_t;
+
 void print_all(const char * const format, ...);
 
 #endif
