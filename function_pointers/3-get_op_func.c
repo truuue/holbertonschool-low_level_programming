b2ca1afb6348f3adaa6cad3b73628952c2d9355e
truuue <noahvernhet@gmail.com>
2023-03-24 06:09:06 -0700

    Adding all the calc files

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function
+ * @s: operator
+ * Return: pntr to func
+ */
+
+int (*get_op_func(char *s))(int, int)
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (*ops[i].op == *s)
+			return (*ops[i].f);
+
+		i++;
+	}
+	return (NULL);
+}
