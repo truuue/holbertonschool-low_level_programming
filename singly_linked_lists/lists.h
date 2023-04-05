9b700e48077025ab522d47877b42f6ab596516a0
truuue <noahvernhet@gmail.com>
2023-04-05 13:26:52 -0700

    Uptade include in header file

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
