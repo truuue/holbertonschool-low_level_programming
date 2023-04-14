f12c506ecf38cfbbc03141a7961f56d83a341950
truuue <noahvernhet@gmail.com>
2023-04-14 10:32:34 -0700

    Write a function that deletes a hash table / fix coding style

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -7,24 +7,24 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long i = 0;
+	unsigned long i = 0;
 
-    while (i < ht->size)
-    {
-        hash_node_t *node = ht->array[i];
+	while (i < ht->size)
+	{
+		hash_node_t *node = ht->array[i];
 
-        while (node != NULL)
-        {
-            hash_node_t *temp = node;
+		while (node != NULL)
+		{
+			hash_node_t *temp = node;
 
-            node = node->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-        i++;
-    }
-    free(ht->array);
-    free(ht);
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
 
