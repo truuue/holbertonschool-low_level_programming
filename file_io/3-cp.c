76d0889dd961e3aaa35a1f5ae36a109ff984350c
truuue <noahvernhet@gmail.com>
2023-04-14 11:56:10 -0700

    Write a program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,6 +7,21 @@
  * Retrun: 0 on success
  */
 
+void file_error(int fd_from, int fd_to, char *argv[])
+{
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to, i;
@@ -19,33 +34,19 @@ int main(int argc, char **argv)
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+
+	file_error(fd_from, fd_to, argv);
 
 	while ((i = read(fd_from, buf, 1024)) > 0)
 	{
 		if (write(fd_to, buf, i) != i)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			file_error(0, -1, argv);
 	}
 
 	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		file_error(-1, 0, argv);
 
 	if (close(fd_from) == -1)
 	{
@@ -58,6 +59,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return 0;
 }
