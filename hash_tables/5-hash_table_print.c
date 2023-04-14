efb9426cda02c4a47c998a92ab4815de55fb7e7d
truuue <noahvernhet@gmail.com>
2023-04-14 09:02:11 -0700

    Write a function that prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.c"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: is the hash table
+ * @x: check if a key-value pair has already been printed in the hash table
+ * If ht is NULL, don’t print anything
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int x = 0;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	while (i < ht->size)
+	{
+		temp = ht->array[i];
+		if (temp != NULL)
+		{
+			if (start == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", temp->key, temp->value);
+			while ((temp = temp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+			}
+
+			start = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
