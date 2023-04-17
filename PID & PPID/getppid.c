3ed22b44b8a63de00feb781bef80855aa85cee8e
truuue <noahvernhet@gmail.com>
2023-04-17 01:48:52 -0700

    get PID

--- PID & PPID/getppid.c
+++ PID & PPID/getppid.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+
 int main()
 {
     int p_id, p_pid;
@@ -8,8 +14,8 @@ int main()
     p_id = getpid(); /*process id*/
     p_pid = getpid(); /*parent process id*/
 
-    printf("Process ID: %d\n", p_id);
-    printf("Parent Process ID: %d\n", p_pid);
+    printf("Process ID: %u\n", p_id);
+    printf("Parent Process ID: %u\n", p_pid);
 
     return 0;
 }
