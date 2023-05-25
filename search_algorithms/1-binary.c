2a3860e003fe8f5d72e4444679fb6f33e7c70231
truuue <noahvernhet@gmail.com>
2023-05-25 10:35:05 -0700

    Betty/kernel fix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,29 +1,36 @@
 #include "search_algos.h"
 
+/**
+ * binary_search - searches value in int array using Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: is the value to search for
+ * Return: the index where value is located, if array is NULL return -1
+*/
 int binary_search(int *array, size_t size, int value)
 {
-    int n;
-    int mid;
+	int n;
+	int mid;
 	int low = 0;
 	int high = size - 1;
 
 	if (array == NULL)
-    {
+	{
 		return (-1);
-    }
+	}
 
 	while (low <= high)
 	{
 		mid = (low + high) / 2;
 		printf("Searching in array: ");
 
-        n = low;
+		n = low;
 		while (n <= high)
 		{
 			if (n > low)
 				printf(", ");
 			printf("%d", array[n]);
-            n++;
+			n++;
 		}
 		printf("\n");
 
